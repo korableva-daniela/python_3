87d9f9a82da49e049f11239ab4c4367adee29940
korableva-daniela <113258230+korableva-daniela@users.noreply.github.com>
2024-02-25 00:14:54 +0300

    lab6 Task-3 filling in the database

--- lab6_1/main.py
+++ lab6_1/main.py
@@ -38,3 +38,40 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS  take_a_book
             	FOREIGN KEY (id_students) REFERENCES student(id_student));''')
 
 conn.commit()
+cursor.execute("""INSERT INTO student(id_student, name, surname, age, course,faculty) 
+   VALUES('00001', 'Александр','Невский', '20', '2','Исторический');""")
+cursor.execute("""INSERT INTO student(id_student, name, surname, age, course,faculty) 
+   VALUES('00002', 'Дмитрий','Менделеев', '21', '3','Химический');""")
+
+
+
+cursor.execute("""INSERT INTO book(id_book, name_book , author ,year_of_publication)
+   VALUES('00011', 'Ромео и Джульетта','Шекспир','1869');""")
+cursor.execute("""INSERT INTO book(id_book, name_book ,author , year_of_publication)
+   VALUES('00012', 'Герой нашего времени','Лермонтов','1840');""")
+
+cursor.execute("""INSERT INTO library(id_library, name_library , address_library)
+   VALUES('00111', 'Библиотека имени Ленина','Москва, ул.Воздвиженка');""")
+cursor.execute("""INSERT INTO library(id_library, name_library , address_library)
+   VALUES('00222', 'Библиотека имени Ломоносова','Санкт-Петербург, ул Нахимова');""")
+
+
+cursor.execute("""INSERT INTO book_in_library(id_book_in_library, copies,id_books,id_libraryes)
+   VALUES('01111', '1000', '00011','00222' );""")
+cursor.execute("""INSERT INTO book_in_library(id_book_in_library, copies,id_books,id_libraryes)
+   VALUES('02222', '200', '00012','00111' );""")
+
+cursor.execute("""INSERT INTO  take_a_book(id_take , number_of_days,id_books,id_students)
+   VALUES('11111', '10', '00011','00002' );""")
+cursor.execute("""INSERT INTO  take_a_book(id_take , number_of_days,id_books,id_students)
+   VALUES('22222', '7', '00012','00001' );""")
+cursor.execute("""INSERT INTO  take_a_book(id_take , number_of_days,id_books,id_students)
+   VALUES('33333', '15', '00012','00002' );""")
+
+sqlite_select_query ="""Select* from student;"""
+
+conn.commit()
+
+
+
+
