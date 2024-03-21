08c4abab17ed6fa90714bbd142a68025bd30d9f8
korableva-daniela <113258230+korableva-daniela@users.noreply.github.com>
2024-03-21 21:42:19 +0300

    Update CGL.py

--- lab6_1/CGL.py
+++ lab6_1/CGL.py
@@ -1,5 +1,1 @@
-from http.server import HTTPServer, CGIHTTPRequestHandler
-port = 8080
-httpd = HTTPServer(('', port), CGIHTTPRequestHandler)
-print("Starting simple_httpd on port: " + str(httpd.server_port)+" http://localhost:8080/")
-httpd.serve_forever()
+
